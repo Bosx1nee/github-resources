f3b2dde24fb6d02e9b86a6869071952ad4c31adf
Bosx1nee <33735478+Bosx1nee@users.noreply.github.com>
2017-11-21 15:24:21 -0700

    updated script

--- _scripts/scripts.js
+++ _scripts/scripts.js
@@ -1,1 +1,21 @@
+var input = document.querySelector("");
+var result = document.querySelector("");
 
+//The new function 
+function userText() {
+    var inputText = document.getElementById("").value;
+    document.getElementById("").innerHTML = inputText;
+}
+
+//Creates the highlight element and tells which color corresponds to how much words there are
+text.oninput = function (){
+    var box = document.createElement("");
+    var sentence = text.value.match(/([^])\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g)
+    console.log(sentences);
+    paragraph.innerHTML = "";
+    
+    for (var i = 0; i <sentence.length; i++);
+    var sentence = sentences[i];
+    var length = sentence.split("").length;
+    highlight.textContent
+}
